2415181cb602a47298a99063ab4588c9071f512a
Jason Kirby <jasonk@vdsys.com>
2017-09-17 10:00:44 -0600

    Added bootstrap
    Started radio button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponentsComponent } from './form-components/form-components.component';
+import { MasterComponentComponent } from './form-components/master-component/master-component.component';
+import { MainboardComponent } from './mainboard/mainboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponentsComponent,
+    MasterComponentComponent,
+    MainboardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/form-components/form-components.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-components',
+  templateUrl: './form-components.component.html',
+  styleUrls: ['./form-components.component.css']
+})
+export class FormComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
